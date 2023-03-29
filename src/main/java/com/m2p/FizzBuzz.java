77f75f3d3745e35ea80e7e9f451f12dae09b915e
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-03-30 00:54:52 +0530

    Req1: Add. Added The Tests And The Method To Check Fizz

--- src/main/java/com/m2p/FizzBuzz.java
+++ src/main/java/com/m2p/FizzBuzz.java
@@ -1,4 +1,13 @@
 package com.m2p;
 
 public class FizzBuzz {
+    public String getFizzBuzz(int inputNumber) {
+        if(inputNumber % 3 == 0){
+            return "Fizz";
+        }
+        else if(inputNumber % 5 == 0){
+            return "Buzz";
+        }else
+            return String.valueOf(inputNumber);
+    }
 }
--- src/test/java/com/m2p/FizzBuzzTest.java
+++ src/test/java/com/m2p/FizzBuzzTest.java
@@ -1,8 +1,24 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
+    FizzBuzz number = new FizzBuzz();
+
+    @Nested
+    class Fizz{
+        @Test
+        void toReturnFizzWhenTheNumberIsTwentyOne(){
+            String expected = "Fizz";
+
+            String actual = number.getFizzBuzz(96);
+
+            assertEquals(expected, actual);
+        }
+    }
 
 }
